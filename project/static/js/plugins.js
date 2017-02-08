85f9081f8728c6d1ce091b86cc3ba1d2dc2c4a8b
Christian Gass <christiangass@outlook.com>
2017-02-07 22:59:02 -0500

    annotation

--- project/static/js/plugins.js
+++ project/static/js/plugins.js
@@ -1,3 +1,6 @@
+/************************************************************************************
+ * place any jQuery/helper plugins in here, instead of separate, slower script files.
+ */
 
 // usage: log('inside coolFunc', this, arguments);
 // paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
@@ -14,7 +17,3 @@ window.log = function(){
 // make it safe to use console.log always
 (function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,timeStamp,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
 {console.log();return window.console;}catch(err){return window.console={};}})());
-
-
-// place any jQuery/helper plugins in here, instead of separate, slower script files.
-
