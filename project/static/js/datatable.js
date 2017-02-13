94fd8700db63c4d20eff3727fc588a1591ac5b3b
Christian Gass <christiangass@outlook.com>
2017-02-12 21:29:28 -0500

    functioning data table
    added jQuery DataTable - used to search for and the select a venue for
    editing

--- project/static/js/datatable.js
+++ project/static/js/datatable.js
@@ -2,46 +2,36 @@
  * create the dataTable from the CartoDB SQL API response
  */
 $(document).ready(function() {
-    var response = $('#data-table').DataTable({
+    var table = $('#data-table').DataTable({
+        processing: true,
+        dom: 'lfrtipB',
         ajax:  {
             url: "https://christianbgass.carto.com/api/v2/sql?q=SELECT venue_name, venue_address, website, phone, email, validated, publish, cartodb_id FROM fishfrymap",
             type: 'GET',
             dataSrc: 'rows'
         },
-        /*
-        "columnDefs": [{
-            "defaultContent": "-",
-            "targets": "_all"
-        }]
-        */
-        //"order": [[0, "asc"]],
         columns : [
-            {"rows": 'venue_name'},
-            {"rows": 'venue_address'},
-            {"rows": 'website'},
-            {"rows": 'phone'},
-            {"rows": 'email'},
-            {"rows": 'validated'},
-            {"rows": 'publish'},
-            {"rows": 'cartodb_id'}
-            /*
-            {rows: 'alcohol'},
-            {rows: 'ash_wed'},
-            {rows: 'etc'},
-            {rows: 'good_fri'},
-            {rows: 'handicap'},
-            {rows: 'homemade_pierogies'},
-            {rows: 'lunch'},
-            {rows: 'men{data: '},
-            {rows: 'publish'},
-            {rows: 'take_out'},
-            {rows: 'the_geom'},
-            {rows: 'the_geom_webmercator'},
-            {rows: 'venue_notes'},
-            {rows: 'venue_type'},
-            {rows: 'z_desc_2016'}
-            */
-        ]
+            {data: 'venue_name'},
+            {data: 'venue_address'},
+            {data: 'website'},
+            {data: 'phone'},
+            {data: 'email'},
+            {data: 'validated'},
+            {data: 'publish'},
+            {data: 'cartodb_id'}
+        ],
+        select: {
+            style: 'single'
+        },
+        buttons: [
+             {
+                 extend: 'selected',
+                 text: 'Edit selected Fish Fry',
+                 action: function (e, dt, button, config) {
+                     alert( dt.rows( { selected: true } ).indexes().length +' row(s) selected' );
+                 }
+             }
+         ]
     });
-    //console.log(response);
 });
+
