6958426afd99e70827be13cba36d6285f029186f
Christian Gass <christiangass@outlook.com>
2017-02-15 10:44:54 -0500

    add'l tests

--- tests/carto_api.py
+++ tests/carto_api.py
@@ -15,18 +15,29 @@ r = requests.get(api_url, params=payload)
 pp.pprint(json.loads(r.text))
 
 # test 2
-q = '''SELECT * FROM fishfrymap INNER JOIN fishfry_dt ON fishfrymap.cartodb_id=fishfry_dt.venue_key '''
+q = '''SELECT * FROM fishfrymap INNER JOIN fishfry_dt ON fishfrymap.cartodb_id=fishfry_dt.venue_key'''
 r = requests.get(api_url, params= {'q': q,'api_key': api_key})
 pp.pprint(json.loads(r.text))
 
-q = '''SELECT * FROM fishfrymap INNER JOIN fishfry_dt ON fishfrymap.cartodb_id=fishfry_dt.venue_key WHERE fishfry_venues.cartodb_id = 1'''
-r = requests.get(api_url, params= {'q': q,'api_key': api_key})
-pp.pprint(json.loads(r.text))
+# test 3
+q = '''SELECT * FROM fishfrymap INNER JOIN fishfry_dt ON fishfrymap.cartodb_id=fishfry_dt.venue_key WHERE fishfrymap.cartodb_id = 1'''
+r = requests.get(api_url, params= {'q': q,'api_key': api_key}).text
 
+
+pp.pprint(json.loads(r))
+
+# test 4
 q = '''SELECT dt, dt_init, venue_key FROM fishfrymap INNER JOIN fishfry_dt ON fishfry_venues.cartodb_id=fishfry_dt.venue_key WHERE fishfrymap.cartodb_id = 1'''
 r = requests.get(api_url, params= {'q': q,'api_key': api_key})
 pp.pprint(json.loads(r.text))
 
+# test 5
 q = """INSERT INTO fishfry_dt (dt) VALUES ('2017-03-21 13:00')"""
 r = requests.get(api_url, params= {'q': q,'api_key': api_key})
 pp.pprint(json.loads(r.text))
+
+# test 6
+pp.pprint(json.loads(requests.put("http://localhost:5000/api/fishfrys/201", params={"name":"The Name","type":"The Type"}).text))
+pp.pprint(json.loads(requests.delete("http://localhost:5000/api/fishfrys/201").text))
+
+[{ u'cartodb_id': 8, u'dt_end': u'2017-02-14T16:57:35Z',u'dt_start': u'2017-02-14T16:57:39Z',u'venue_key': 1},{ u'cartodb_id': 9,u'dt_end': u'2017-02-14T17:00:03Z',u'dt_start':u'2017-02-14T17:00:02Z',u'venue_key': 1}]
