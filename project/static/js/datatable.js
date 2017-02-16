120f6a30f85f1e46c4a4dc78046c03fe153bb739
Christian Gass <christiangass@outlook.com>
2017-02-16 17:08:48 -0500

    added linked edit button to data table

--- project/static/js/datatable.js
+++ project/static/js/datatable.js
@@ -4,12 +4,13 @@
 $(document).ready(function() {
     var table = $('#data-table').DataTable({
         processing: true,
-        dom: 'lfrtipB',
+        dom: 'lfrtip',
         ajax:  {
             url: "https://christianbgass.carto.com/api/v2/sql?q=SELECT venue_name, venue_address, website, phone, email, validated, publish, cartodb_id FROM fishfrymap",
             type: 'GET',
             dataSrc: 'rows'
         },
+        deferRender: true,
         columns : [
             {data: 'venue_name'},
             {data: 'venue_address'},
@@ -21,17 +22,50 @@ $(document).ready(function() {
             {data: 'cartodb_id'}
         ],
         select: {
-            style: 'single'
+            style: 'single',
+            blurable: true
         },
         buttons: [
              {
                  extend: 'selected',
-                 text: 'Edit selected Fish Fry',
+                 text: 'Edit Selected Fish Fry'
+                 /*
                  action: function (e, dt, button, config) {
-                     alert( dt.rows( { selected: true } ).indexes().length +' row(s) selected' );
+                     //alert( dt.rows( { selected: true } ).indexes().length +' row(s) selected' );
+                     var x = dt.rows({ selected: true }).data();
+                     console.log(JSON.stringify(x[0].cartodb_id));
                  }
+                 */
              }
          ]
     });
+
+    $('#data-table').on( 'click', 'tr', function () {
+        if ( $(this).hasClass('selected') ) {
+            var row = table.row('.selected').data();
+            //console.log(row.cartodb_id);
+            route = "/contribute/fishfry/" + row.cartodb_id;
+            $('#editbutton').attr("disabled", false);
+            $('#editbutton').attr("href",route);
+            $('#editbutton').text("Edit Selected Fish Fry (" + row.cartodb_id + ")");
+        } else {
+            $('#editbutton').attr("disabled", true);
+        }
+        /*
+        if ( $(this).hasClass('selected') ) {
+            $(this).removeClass('selected');
+        }
+        else {
+            table.$('tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+        }
+        */
+    } );
+    
+    $('#editbutton').click( function() {
+            var row = table.row('.selected').data();
+            console.log(row.cartodb_id);
+        });
+    
 });
 
