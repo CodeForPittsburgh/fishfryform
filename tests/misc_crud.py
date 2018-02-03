1ce0b9d8436d23b66b1835618d7499f0c60b1bc6
Christian Gass <christiangass@outlook.com>
2018-02-02 22:11:02 -0500

    unit tests

--- tests/misc_crud.py
+++ tests/misc_crud.py
@@ -1,6 +1,10 @@
+"""
+explores some boto3 calls with this data
+"""
+
 import os
 import json
-import uuid 
+import uuid
 import boto3
 import decimal
 from boto3.dynamodb.conditions import Key, Attr
@@ -9,7 +13,7 @@ from botocore.exceptions import ClientError
 from core.api import fishfry_table, DecimalEncoder
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-feature_json = os.path.join(dir_path,r'feature.json')
+feature_json = os.path.join(dir_path, r'feature.json')
 
 
 # Load feature
@@ -36,7 +40,7 @@ print(json.dumps(response, indent=2, cls=DecimalEncoder))
 
 # retrieve that record
 try:
-    response = fishfry_table.get_item(Key={'id':ffid,'type':"Feature"})
+    response = fishfry_table.get_item(Key={'id': ffid, 'type': "Feature"})
 except ClientError as e:
     print(e.response['Error']['Message'])
 else:
@@ -45,12 +49,12 @@ else:
         print("GetItem succeeded:")
         print(json.dumps(item, indent=2, cls=DecimalEncoder))
     else:
-        print("GetItem did not succeed: No item found")    
+        print("GetItem did not succeed: No item found")
 
 
 # update some things
 response = fishfry_table.update_item(
-    Key = {'id':ffid,'type':"Feature"},
+    Key={'id': ffid, 'type': "Feature"},
     UpdateExpression="set properties.venue_address = :v, properties.publish=:p",
     ExpressionAttributeValues={
         ':v': "1234 Anywhere Street",
@@ -64,7 +68,7 @@ print(json.dumps(response, indent=2, cls=DecimalEncoder))
 
 # update some things
 response = fishfry_table.update_item(
-    Key = {'id':ffid,'type':"Feature"},
+    Key={'id': ffid, 'type': "Feature"},
     UpdateExpression="set properties = :p",
     ExpressionAttributeValues={
         ':p': feature['properties']
@@ -77,7 +81,7 @@ print(json.dumps(response, indent=2, cls=DecimalEncoder))
 
 try:
     response = fishfry_table.delete_item(
-        Key={'id':ffid,'type':"Feature"}
+        Key={'id': ffid, 'type': "Feature"}
     )
 except ClientError as e:
     print(e.response['Error']['Message'])
--- tests/misc_schema.py
+++ tests/misc_schema.py
@@ -1,10 +1,16 @@
-import os, json
+"""
+explores the marshmallow schema
+"""
+
+import os
+import json
 from core import models
 from marshmallow import pprint
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-feature = json.load(open(os.path.join(dir_path,r'feature.json'),'r'))
-features = json.load(open(os.path.join(dir_path,r'features.json'),'r'))['features']
+feature = json.load(open(os.path.join(dir_path, r'feature.json'), 'r'))
+features = json.load(open(os.path.join(dir_path, r'features.json'), 'r'))[
+    'features']
 
 schema = models.FishFry()
 result = schema.load(feature)
@@ -17,4 +23,4 @@ result = schema.load(feature)
 for feature in features:
     data, errors = models.FishFry().load(feature)
     if errors:
-        print("\n",feature['id'], errors)
+        print("\n", feature['id'], errors)
