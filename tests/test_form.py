ee62bcfa7af5a4360cfde2471607b538636ecd4b
Christian Gass <christiangass@outlook.com>
2018-02-06 13:06:05 -0500

    form tests

--- tests/test_form.py
+++ tests/test_form.py
@@ -12,10 +12,15 @@ import unittest
 import uuid
 from random import sample
 from datetime import date
+from dateutil.parser import parse
 
 from core import application, api, application_db, dynamo_db
 from core.config import basedir
 from core.models import FishFryFeature, FishFryProperties, FishFryEvent, FishFryMenu, FeatureCollection, Feature
+from core.forms import FishFryForm, EventForm, postprocess_events
+from core.forms import postprocess_boolean as postbool
+from core.forms import preprocess_boolean as prebool
+from core.utils import sort_records, handle_utc
 
 # workaround for boto3 bug when creating tables in dynamodb
 os.environ["TZ"] = "UTC"
@@ -102,6 +107,10 @@ class APITests(unittest.TestCase):
         features = sample(gj['features'], sample_size)
         return features
 
+    def get_json(self, filepath):
+        with open(filepath) as json_file:
+            return json.load(json_file, parse_float=decimal.Decimal)
+
     def add_many_random_features(self, sample_size=10):
         """add test geojson features to a dynamo db table
         """
@@ -138,6 +147,49 @@ class APITests(unittest.TestCase):
         )
         return feature
 
+    def load_feature_to_form(self, onefry):
+        with self.testapp.application.app_context():
+            form = FishFryForm()
+            # shortcut to the returned fish fry's properties
+            p = onefry['properties']
+            # map the fish fry data to the form fields
+            form.ffid.data = onefry['id']
+            form.alcohol.data = prebool(p['alcohol'])
+            form.email.data = p['email']
+            form.etc.data = p['etc']
+            form.handicap.data = prebool(p['handicap'])
+            form.homemade_pierogies.data = prebool(p['homemade_pierogies'])
+            form.lunch.data = prebool(p['lunch'])
+            form.menu_txt.data = p['menu']['text']
+            form.menu_url.data = p['menu']['url']
+            form.phone.data = p['phone']
+            form.publish.data = p['publish']
+            form.take_out.data = prebool(p['take_out'])
+            form.validated.data = ['validated']
+            form.venue_address.data = p['venue_address']
+            form.venue_name.data = p['venue_name']
+            form.venue_notes.data = p['venue_notes']
+            form.venue_type.data = p['venue_type']
+            form.website.data = p['website']
+            try:
+                form.lng.data = onefry['geometry']['coordinates'][0]
+                form.lat.data = onefry['geometry']['coordinates'][1]
+            except:
+                form.lng.data = None
+                form.lat.data = None
+
+            if p['events']:
+                events = sort_records(p['events'], 'dt_start')
+                for event in events:
+                    event_form = EventForm()
+                    event_form.dt_start = parse(event['dt_start'])
+                    event_form.dt_end = parse(event['dt_end'])
+                    form.events.append_entry(event_form)
+
+            print(dir(form))
+            print(form.__dict__)
+            return form.to_dict()
+
     # -------------------------------------------------------------------------
     # TESTS
 
@@ -159,7 +211,21 @@ class APITests(unittest.TestCase):
         self.assertIn(b'Edit an Existing Fish Fry', response.data)
         # self.assertIn(str.encode(venue_name), response.data)
 
-    def test_submit_a_new_fry(self):
+    def test_load_existing_invalid_fry(self):
+        """load a feature that we know is broken
+        """
+        feature = self.get_json(os.path.join(
+            tests_path, "feature_invalid_geom.json")
+        )
+        ffid = feature['id']
+        venue_name = feature['properties']['venue_name']
+        response = self.testapp.get(
+            '/edit/?ffid={0}'.format(ffid)
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(str.encode(venue_name), response.data)
+
+    def submit_a_new_fry(self):
         """submit a new fry to the endpoint. No ffid.
         Page re-renders, with flask-flash message with status
         """
@@ -171,18 +237,34 @@ class APITests(unittest.TestCase):
         self.assertIn(b'FeatureCollection', response.data)
         self.assertIn(b'features', response.data)
 
-    def submit_an_update(self):
+    def test_submit_an_update(self):
         """submit an updated fry to the endpoint. Has ffid
         Page re-renders, with flask-flash message with status.
         """
-        feature = self.get_random_features(1)[0]
+        feature = self.get_json(os.path.join(
+            tests_path, "feature_invalid_geom.json"))
+        ffid = feature['id']
 
-        response = self.testapp.get('/api/fishfries/')
+        # add some things to the data
+        feature['geometry'] = {
+            "type": "Point",
+            "coordinate": [-80.218302, 40.223398]
+        }
+        new_data = "This is a test note"
+        feature['properties']['etc'] = new_data
+        # load it to a form
+        form = self.load_feature_to_form(feature)
+        # submit to endpoint
+        self.testapp.post(
+            'submit/',
+            data=form,
+            follow_redirects=True
+        )
+        # see if data is there
+        response = self.testapp.get(
+            '/edit/?ffid={0}'.format(ffid))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.mimetype, 'application/json')
-        # data, errors = FeatureCollection().load(response.data)
-        self.assertIn(b'FeatureCollection', response.data)
-        self.assertIn(b'features', response.data)
+        self.assertIn(str.encode(new_data), response.data)
 
 
 if __name__ == '__main__':
