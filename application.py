e328f323c12d9a7898d0594a3547a97f019adde1
Christian Gass <christiangass@outlook.com>
2018-02-06 13:05:56 -0500

    debugging help

--- application.py
+++ application.py
@@ -5,7 +5,13 @@ main entry point for the application
 
 from core import application
 
+debug = application.config['DEBUG']
+
+if debug:
+    import win_unicode_console
+
 # Default port:
 if __name__ == '__main__':
-    application.run(debug=True)
-    
+    if debug:
+        win_unicode_console.enable()
+    application.run(debug=debug)
