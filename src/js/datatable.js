151cc770a1e8e00b8387abfc9c07a0a487b489f5
gassc <christian.gass@outlook.com>
2019-02-27 10:43:28 -0500

    link table select with map popup

--- src/js/datatable.js
+++ src/js/datatable.js
@@ -5,16 +5,43 @@ require("datatables.net-select-bs")(window, $);
 var L = require("leaflet");
 
 $(function () {
-    var map, fishfries;
 
+    var map, fishfryLayer, fishfryTable, refMunis;
+
+    /** -----------------------------------------
+     * MAP SETUP
+     */
     map = new L.Map("map", {
         center: [40.440734, -80.0091294],
         zoom: 10
     });
 
+    L.tileLayer(
+        "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
+            maxZoom: 18,
+            attribution: 'Tiles via <a href="http://carto.com">Carto</a>. Basemap data from <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a> license.'
+        }
+    ).addTo(map);
+
+    // $.getJSON("http://services1.arcgis.com/jOy9iZUXBy03ojXb/arcgis/rest/services/PennDOTBoundaries_v_2/FeatureServer/2/query?where=1%3D1&f=pgeojson&outSR=4326", (data) => {
+    //     console.log("get")
+    //     refMunis = L.geoJSON(data, {}).addTo(map);
+    // })
+
+
+    /** -----------------------------------------
+     * LOAD DATA
+     * get the fish fry data and add it to the map and table
+     */
+
     $.getJSON(Flask.url_for("api") + "fishfries/", function (data) {
+
         console.log("loaded:", data);
-        fishfries = L.geoJSON(data, {
+
+        // --------------------------------------
+        // FISH FRY map layer
+
+        fishfryLayer = L.geoJSON(data, {
             style: function (feature) {
                 if (feature.properties.publish && feature.properties.validated) {
                     return { color: "#FCB82E" };
@@ -42,101 +69,89 @@ $(function () {
                     )
                 );
             }
+        }).addTo(map);
+
+        var lookupLayerIdFfid = {}
+        var lookupFfidLayerId = {}
+        Object.keys(fishfryLayer._layers).forEach((k) => {
+            lookupLayerIdFfid[k] = fishfryLayer._layers[k].feature.id;
+            lookupFfidLayerId[fishfryLayer._layers[k].feature.id] = k;
         });
-        fishfries.addTo(map);
-    });
 
-    L.tileLayer(
-        "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
-            maxZoom: 18,
-            attribution: 'Tiles via <a href="http://carto.com">Carto</a>. Basemap data from <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a> license.'
-        }
-    ).addTo(map);
+        // --------------------------------------
+        // FISH FRY data table
 
-});
-
-/**
- * create the dataTable from the API Response
- */
-$(document).ready(function () {
-
-    $(".editbutton").click(function () {
-        var row = table.row(".selected").data();
-        console.log(row.ffid);
-    });
-
-    var table = $("#data-table").DataTable({
-        processing: true,
-        dom: "lfrtip",
-        ajax: {
-            url: Flask.url_for("api") + "fishfries/",
-            type: "GET",
-            dataSrc: "features"
-        },
-        deferRender: true,
-        columns: [
-            {
-                data: "properties.venue_name"
-            }, {
-                data: "properties.venue_address"
-            }, {
-                data: "properties.website",
-                render: function (data, type, row, meta) {
-                    if (data) {
-                        return '<a href="' + data + '" target="_blank">Website</a>';
-                    } else {
-                        return "";
+        fishfryTable = $("#data-table").DataTable({
+            processing: true,
+            dom: "lfrtip",
+            data: data.features,
+            deferRender: true,
+            columns: [
+                {
+                    data: "properties.venue_name"
+                }, {
+                    data: "properties.venue_address"
+                }, {
+                    data: "properties.website",
+                    render: function (data, type, row, meta) {
+                        if (data) {
+                            return '<a href="' + data + '" target="_blank">Website</a>';
+                        } else {
+                            return "";
+                        }
                     }
+                }, {
+                    data: "properties.validated"
+                }, {
+                    data: "properties.publish"
                 }
-            }, {
-                data: "properties.validated"
-            }, {
-                data: "properties.publish"
+            ],
+            select: {
+                style: "single"
+                // blurable: true
+            },
+            buttons: [{
+                extend: "selected",
+                text: "Edit Selected Fish Fry"
+            }]
+        });
+
+        fishfryTable.on("select", function (e, dt, type, indexes) {
+            if (type === "row") {
+
+                // enable the edit button 
+
+                var row = fishfryTable.rows(indexes).data();//.pluck('id');
+                var feature = row['0'];
+                $(".editbutton").attr("disabled", false);
+                $(".editbutton").attr("href", Flask.url_for("load_fishfry", {
+                    ffid: feature.id
+                }));
+                $(".editbutton").text(
+                    "Edit: " + feature.properties.venue_name
+                );
+
+                // open the map popup
+                map._layers[lookupFfidLayerId[feature.id]].fire('click');
+
             }
-        ],
-        select: {
-            style: "single",
-            blurable: true
-        },
-        buttons: [{
-            extend: "selected",
-            text: "Edit Selected Fish Fry"
-            // action: function(e, dt, button, config) {
-            //     //alert( dt.rows( { selected: true } ).indexes().length +' row(s) selected' );
-            //     var x = dt.rows({ selected: true }).data();
-            //     console.log(JSON.stringify(x[0].cartodb_id));
-            // }
-        }]
-    });
+        });
 
-    table.on("select", function (e, dt, type, indexes) {
-        if (type === "row") {
-            var row = table.rows(indexes).data();//.pluck('id');
-            var feature = row['0'];
-            // dynamically set the route for the edit button from selected row
-            // route = "/contribute/fishfry/" + row.ffid;
-            var route = Flask.url_for("load_fishfry", {
-                ffid: feature.id
-            });
-            $(".editbutton").attr("disabled", false);
-            $(".editbutton").attr("href", route);
-            $(".editbutton").text(
-                "Edit: " + feature.properties.venue_name
-            );
-            console.log(route);
-        }
-        // } else {
-        //     $(".editbutton").attr("disabled", true);
-        // }
-    });
+        fishfryTable.on("deselect", function (e, dt, type, indexes) {
+            if (type === "row") {
 
-    table.on("deselect", function (e, dt, type, indexes) {
-        if (type === "row") {
-            $(".editbutton").attr("disabled", true);
-            $(".editbutton").text(
-                "Edit Selected Fish Fry"
-            );
-        }
+                // disable edit button
+
+                $(".editbutton").attr("disabled", true);
+                $(".editbutton").text(
+                    "Edit Selected Fish Fry"
+                );
+
+                // deselect on the map
+                map.closePopup();
+            }
+        });
+
     });
 
 });
